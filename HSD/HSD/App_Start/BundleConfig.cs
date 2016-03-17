4bbcb2bfc1f3f2ba2a658f7b934405dca985a7fe
sorooshansari <sorosh.ansari@gmaill.com>
2016-03-18 02:29:44 +0330

    3/18/2016 2:29

---
+++ HSD/HSD/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace HSD
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/HSDcss").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
+                      "~/Content/style-library-1.css",
+                      "~/Content/plugins.css",
+                      "~/Content/blocks.css",
+                      "~/Content/custom.css"));
+            bundles.Add(new ScriptBundle("~/bundles/HSDjs").Include(
+                      "~/Scripts/jquery-2.2.1.min.js",
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/plugins.js",
+                      "~/Scripts/skit-scripts.js",
+                      "~/Scripts/custom.js"));
+        }
+    }
+}
---
+++ HSD/HSD/Scripts/custom.js
@@ -0,0 +1,32 @@
+﻿$(window).scroll(function () {
+    var scrtop = $(document).scrollTop();
+    $('#scrolltxt').html(scrtop.toString());
+    if (scrtop > 399) {
+        $('nav').css('height', 65);
+        $('.navbar-nav').css('margin-top', 10);
+        $('.brand-img').css('height', 45);
+        $('.brand-img').css('margin-top', 10);
+        $('.hero').css('font-size', 27);
+        $('.sustainable').css('font-size', 25);
+        $('.development').css('font-size', 23);
+        $('.navbar-nav > li a').css('font-size', 12);
+        $('.navbar-toggle').css('top', 15);
+    } else {
+        $('nav').css('height', 102);
+        $('.navbar-nav').css('margin-top', 25);
+        $('.brand-img').css('height', 80);
+        $('.brand-img').css('margin-top', 12);
+        $('.hero').css('font-size', 34);
+        $('.sustainable').css('font-size', 32);
+        $('.development').css('font-size', 30);
+        $('.navbar-nav > li a').css('font-size', 15);
+        $('.navbar-toggle').css('top', 30);
+    }
+});
+function btnrotate() {
+    if ($('#navbar-btn').css("transform") == 'none') {
+        $('#navbar-btn').css("transform", "rotate(-90deg)");
+    } else {
+        $('#navbar-btn').css("transform", "");
+    }
+};
